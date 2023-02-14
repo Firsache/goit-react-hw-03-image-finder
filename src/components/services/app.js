8f4ae44db646973f3bacc1e77ed7d22de7980629
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-14 14:55:59 +0200

    created fetchImages async function

--- src/components/services/app.js
+++ src/components/services/app.js
@@ -1,19 +1,34 @@
-// імпортувати axious
+import axios from 'axios';
 
+const API_KEY = '32927001-bdf8cd2697946056c57f9b5f5';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export const fetchImages = async (query, page) {
+    const params = {
+    q: { query },
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: { page },
+    per_page: 12,
+    key: API_KEY
+};
+    const { data } = await axios.get(BASE_URL, {params});
+    return data;
+}
+
+// =======================================================================================
 // const API_KEY = '32927001-bdf8cd2697946056c57f9b5f5';
-// const BASE_URL = 'https://pixabay.com/api/';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// axios.defaults.params = {
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//     per_page: 12,
+//     key: API_KEY
+// };
 
-// const fetchImages = (query, page) {
-
-// const params =
-//             {
-//                 q: this.query,
-//                 image_type: 'photo',
-//                 orientation: 'horizontal',
-//                 safesearch: true,
-//                 page: this.state.page,
-//                 per_page: this.state.per_page,
-//                 key: PixabayAPI.API_KEY
-//             }
-
-// ${BASE_URL},{params}
+// export const fetchImages =  async (query, page) {
+//     const { data } = await axios.get(`?q=${query},page=${page}`);
+//     return data;
+// }
