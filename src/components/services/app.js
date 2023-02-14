4b2e74e69cc8344fbc8ada19b1709dbe565e8160
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-14 14:36:37 +0200

    Create app.js

---
+++ src/components/services/app.js
@@ -0,0 +1,19 @@
+// імпортувати axious
+
+// const API_KEY = '32927001-bdf8cd2697946056c57f9b5f5';
+// const BASE_URL = 'https://pixabay.com/api/';
+
+// const fetchImages = (query, page) {
+
+// const params =
+//             {
+//                 q: this.query,
+//                 image_type: 'photo',
+//                 orientation: 'horizontal',
+//                 safesearch: true,
+//                 page: this.state.page,
+//                 per_page: this.state.per_page,
+//                 key: PixabayAPI.API_KEY
+//             }
+
+// ${BASE_URL},{params}
