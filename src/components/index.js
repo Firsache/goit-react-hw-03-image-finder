eaf9155bb48570ca9417ca9f90ecd82d72939409
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-14 15:31:25 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export * from 'components/Searchbar/Searchbar.jsx';
 // export * from 'components/Button/Button.jsx';
-// export * from 'components/ImageGallery/ImageGallery.jsx';
+export * from 'components/ImageGallery/ImageGallery.jsx';
