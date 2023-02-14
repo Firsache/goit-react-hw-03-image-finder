8ccafa5d7f508172ba6944d8b417495db44cc07e
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-14 14:55:25 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,17 @@
 import { Component } from 'react';
 import { SearchBar } from 'components/index';
+import { fetchImages } from 'components/services/app';
 
 export class App extends Component {
   state = {
     searchedValue: '',
   };
+  componentDidMount() {
+    fetchImages();
+  }
+  // fetchImages = (query, page) {
+
+  // }
 
   gerSearchedValue = searchedValue => {
     this.setState({ searchedValue });
