be19c2156eb1b5c074a575b10bee35a41c67f381
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-15 00:18:16 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,11 +2,16 @@ import PropTypes from 'prop-types';
 import { GalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { List } from './ImageGallery.styled';
 
-export function GalleryList({ images }) {
+export function GalleryList({ images, toggleModal }) {
   return (
     <List>
       {images.map(({ id, webformatURL, largeImageURL }) => (
-        <GalleryItem key={id} src={webformatURL} alt={largeImageURL} />
+        <GalleryItem
+          key={id}
+          src={webformatURL}
+          alt={largeImageURL}
+          toggleModal={toggleModal}
+        />
       ))}
     </List>
   );
